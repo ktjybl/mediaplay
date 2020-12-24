5ed2aa297c2b9845d8ae8ac66bc54d678e038ece
ktjybl <64632527+ktjybl@users.noreply.github.com>
2020-12-24 13:22:31 +0200

    Add files via upload

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,63 @@
+import{addZero} from './supScript.js';
 export const videoPlayerInit=()=>{
-     console.log('Video Init'); 
+    const videoPlayer=document.querySelector('.video-player'),
+          videoButtonPlay=document.querySelector('.video-button__play'),
+          videoButtonStop=document.querySelector('.video-button__stop'),
+          videoTimePassed=document.querySelector('.video-time__passed'),
+          videoProgress=document.querySelector('.video-progress'),
+          videoTimeTotal=document.querySelector('.video-time__total');
+    const toggleIcon=()=>{
+        if(videoPlayer.paused){
+            videoButtonPlay.classList.remove('fa-pause');
+            videoButtonPlay.classList.add('fa-play');
+        }else{
+            videoButtonPlay.classList.add('fa-pause');
+            videoButtonPlay.classList.remove('fa-play');
+        }
+    }
+    const togglePlay=()=>{
+         ()=>{
+        if(videoPlayer.paused){
+        videoPlayer.play();
+         }else{
+        videoPlayer.pause();
+        }
+    }
+       toggleIcon();
+    };
+    const stopPlay=()=>{
+        videoPlayer.pause();
+        videoPlayer.currentTime=0;
+    };
+
+    videoPlayer.addEventListener('click', togglePlay);
+    videoButtonPlay.addEventListener('click', togglePlay);
+
+    videoPlayer.addEventListener('play', toggleIcon);
+    videoPlayer.addEventListener('pause', toggleIcon);
+
+    videoButtonStop.addEventListener('click', stopPlay);
+
+    videoPlayer.addEventListener('timeupdate', ()=>{
+        const currentTime=videoPlayer.currentTime;
+        const duration=videoPlayer.duration;
+        
+        videoProgress.nodeValue(currentTime/duration)*100;
+
+        let minutePassed= Math.floor(currentTime / 60);
+        let secondsPassed=Math.floor(currentTime % 60);
+
+        let minuteTotal= Math.floor(duration / 60);
+        let secondsTotal=Math.floor(duration % 60);
+        
+        videoTimePassed.textContent=`$(addZero(minutePassed)):$(addZero(secondsPassed))`;
+        videoTimeTotal.textContent=`$(addZero(minuteTotal)):$(addZero(secondsTotal))`;
+     });
+     
+    videoProgress.addEventListener('change', ()=>{
+        const duration=videoPlayer.duration;
+        const value=videoProgress.value;
+
+        videoPlayer.currentTime=(value*duration)/100;
+     });
 }
